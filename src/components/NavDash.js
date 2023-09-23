711160186761713bf9b50ffe339ce2a8145774e5
MikeInsano <snowiestax72@gmail.com>
2023-09-23 20:02:21 +0100

    Bolon Cifuentes / Poner estilos al sidebar

--- src/components/NavDash.js
+++ src/components/NavDash.js
@@ -1,37 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import { HiArrowLongLeft } from "react-icons/hi2";
+import { TiWeatherCloudy } from "react-icons/ti";
+import { FiLoader } from "react-icons/fi";
 
 function NavDash() {
+    const [open, setOpen] = useState(true);
     return (
         <>
-            <div class="bg-[#212121] border-r border-[#e5e5e5] fixed top-0 bottom-0 h-full w-[160px]">
+            <div class={`bg-dark-purple fixed top-0 bottom-0 h-screen pt-8 ${open ? "w-60": "w-20"} duration-300`}>
+                <div>
+                    <HiArrowLongLeft className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-4 border
+                    border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
+                </div>
                 <ul class="m-5">
-                    <li>
-                        <a class="flex items-center space-x-2 py-2 px-4 hover:bg-black cursor-pointer"> 
-                            <span class="text-white">Atmosferica</span>
-                        </a>
+                    <li className="text-gray-300 flex items-center gap-x-4 p-2 hover:bg-light-white 
+                    rounded-md mt-2 cursor-pointer">
+                        <span className="text-3xl block float-left"><TiWeatherCloudy/></span>
+                        <span className={`text-base font-medium flex-1 ${!open && "hidden"}`}>Atmosferica</span>
                     </li>
                 </ul>
                 <ul class="m-5">
-                    <li>
-                        <a class="flex items-center space-x-2 py-2 px-4 hover:bg-black cursor-pointer">
-                            <span class="text-white">NuevoTema</span>
-                        </a>
+                    <li className="text-gray-300 flex items-center gap-x-4 p-2 hover:bg-light-white 
+                    rounded-md mt-2 cursor-pointer">
+                        <span className="text-3xl block float-left"><FiLoader/></span>
+                        <span className={`text-base font-medium flex-1 ${!open && "hidden"}`}>NuevoTema</span>
                     </li>
                 </ul>
                 <ul class="m-5">
-                    <li>
-                        <a class="flex items-center space-x-2 py-2 px-4 hover:bg-black cursor-pointer">
-                            <span class="text-white">NewTheme</span>
-                        </a>
+                    <li className="text-gray-300 flex items-center gap-x-4 p-2 hover:bg-light-white 
+                    rounded-md mt-2 cursor-pointer">
+                        <span className="text-3xl block float-left"><FiLoader/></span>
+                        <span className={`text-base font-medium flex-1 ${!open && "hidden"}`}>NuevoTema</span>
                     </li>
                 </ul>
-                {/* <ul class="m-5 absolute bottom-0 left-0">
-                    <li>
-                        <a class="flex items-center space-x-2 py-2 px-4 hover:bg-black">
-                            <span class="text-white">Logout</span>
-                        </a>
+                <ul class="m-5">
+                    <li className="text-gray-300 flex items-center gap-x-4 p-2 hover:bg-light-white 
+                    rounded-md mt-2 cursor-pointer">
+                        <span className="text-3xl block float-left"><FiLoader/></span>
+                        <span className={`text-base font-medium flex-1 ${!open && "hidden"}`}>NuevoTema</span>
                     </li>
-                </ul> */}
+                </ul>
             </div> 
         </> 
     )
